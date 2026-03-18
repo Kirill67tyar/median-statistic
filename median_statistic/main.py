8bff3930019ff9e39e7ba3afbc1676859caa9469
Kirill67tyar <kirillbogomolov.ric@gmail.com>
2026-03-18 22:18:13 +0400

    Writes load_data function.

--- median_statistic/main.py
+++ median_statistic/main.py
@@ -1,5 +1,21 @@
+import os
 import argparse
 import csv
+import sys
+
+
+def load_data(file_paths: list[str]) -> list[dict]:
+    for path in file_paths:
+        try:
+            with open(path, newline="", encoding="utf-8") as f:
+                return list(csv.DictReader(f))
+        except FileNotFoundError as e:
+            print(e)
+            sys.exit(1)
+        except Exception as e:
+            print(e)
+            sys.exit(1)
+    return []
 
 def main():
     parser = argparse.ArgumentParser(description="Отчёт о потреблении кофе")
@@ -9,6 +25,9 @@ def main():
                         help="Название отчёта (сейчас только median-coffee)")
 
     arguments = parser.parse_args()
+    raw_data = load_data(arguments.files)
+    # path = os.path.abspath(__file__)
+    
     pass
 
 if __name__ == "__main__":
