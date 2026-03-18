83dcbb25b49651dc414e30d1541278ddad4eceed
Kirill67tyar <kirillbogomolov.ric@gmail.com>
2026-03-18 21:03:33 +0400

    Adds arguments to main.py

--- median_statistic/main.py
+++ median_statistic/main.py
@@ -1,5 +1,14 @@
+import argparse
+import csv
 
 def main():
+    parser = argparse.ArgumentParser(description="Отчёт о потреблении кофе")
+    parser.add_argument("--files", nargs="+", required=True,
+                        help="Пути к CSV-файлам (можно несколько)")
+    parser.add_argument("--report", required=True,
+                        help="Название отчёта (сейчас только median-coffee)")
+
+    arguments = parser.parse_args()
     pass
 
 if __name__ == "__main__":
